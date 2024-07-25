075c5b54d8451eddb9e8e10fd764c975f39b9436
Daniel Bauer <daniel.bauer@senckenberg.de>
2024-07-25 15:04:43 +0200

    refactoring

--- app/cordra.py
+++ app/cordra.py
@@ -1,9 +1,11 @@
+import logging
 import os
 from pathlib import Path
 import tempfile
 import magic
 import cordra
 
+logger = logging.getLogger("uvicorn.error")
 
 def create_dataset_from_workflow_artifacts(host, user, password, wfl, artifact_stream_iterator):
     upload_kwargs = {
@@ -16,7 +18,7 @@ def create_dataset_from_workflow_artifacts(host, user, password, wfl, artifact_s
     created_ids = {}
     try:
         # add authors
-        print("Creating authors")
+        logger.debug("Creating authors")
         author1 = cordra.CordraObject.create(obj_type="Person", obj_json={
             "name": "Daniel Bauer",
             "identifier": "https://orcid.org/0000-0001-9447-460X",
@@ -30,60 +32,63 @@ def create_dataset_from_workflow_artifacts(host, user, password, wfl, artifact_s
         created_ids[author2["@id"]] = "Person"
 
         # upload files
-        print("Creating file objects")
+        logger.debug("Creating file objects")
         for file_name, content_iterator in artifact_stream_iterator:
 
-            print("Creating FileObject " + file_name)
+            logger.debug("Creating FileObject from " + file_name)
             relative_path = file_name
 
             # Write file content into temporary file
             # Streaming the file directly into cordra would be better, but this didnt work for me.
             with tempfile.NamedTemporaryFile(delete=True,
                                              prefix=f"argo-artifact-tmp-{os.path.basename(file_name)}-") as tmp_file:
-                print("Downloading content to temp file: " + tmp_file.name)
                 try:
+                    logger.debug("Downloading content to temp file: " + tmp_file.name)
                     for chunk in content_iterator:
                         if chunk:
                             tmp_file.write(chunk)
                 except Exception as e:
+                    logger.error("Failed to download file content: " + str(e))
                     content_iterator.close()
                     raise e
                 tmp_file.flush()
                 file_size = Path(tmp_file.name).stat().st_size
 
-                print(f"Data written to temp file {tmp_file.name} ({file_size / 1024 / 1024:.2f} MB)")
+                logger.debug(f"Download done ({file_size / 1024 / 1024:.2f} MB)")
 
                 # Cordra has issues with huge files
                 if file_size / (1024 * 1024) > 1000:
-                    print(f"File size is {file_size / 1024 / 1024:.2f} MB, which is too large to upload. Skipping...")
+                    logger.warning(f"File size is {file_size / 1024 / 1024:.2f} MB, which is too large to upload. Skipping...")
                     continue
 
                 # figure out file encoding
                 try:
-                    encodingFormat = magic.from_file(tmp_file.name, mime=True)
-                    print("Infered encoding format: " + encodingFormat)
+                    encoding_format = magic.from_file(tmp_file.name, mime=True)
+                    logger.debug("Infered encoding format: " + encoding_format)
                 except magic.MagicException as e:
-                    print(f"Failed to get encoding format for {file_name}")
-                    encodingFormat = None
+                    logger.warning(f"Failed to get encoding format for {file_name}")
+                    encoding_format = None
 
                 # write object
+                logger.debug("Sending file to cordra")
                 file_obj = cordra.CordraObject.create(
                     obj_type="FileObject",
                     obj_json={
                         "name": os.path.basename(relative_path),
                         "contentSize": Path(tmp_file.name).stat().st_size,
-                        "encodingFormat": encodingFormat,
+                        "encodingFormat": encoding_format,
                         "contentUrl": relative_path,
                     },
                     payloads={relative_path: (relative_path, open(tmp_file.name, "rb"))},
                     **upload_kwargs
                 )
+                logger.debug("File ingested")
             created_ids[file_obj["@id"]] = "FileObject"
             break  ## TODO remove to process multiple files
 
         # create action
         # TODO use workflow as action instead of software application
-        print("Create Action")
+        logger.debug("Create SoftwareApplication")
         instrument = cordra.CordraObject.create(
             obj_type="SoftwareApplication",
             obj_json={
@@ -94,6 +99,7 @@ def create_dataset_from_workflow_artifacts(host, user, password, wfl, artifact_s
         )
         created_ids[instrument["@id"]] = "SoftwareApplication"
 
+        logger.debug("Create CreateAction")
         action = cordra.CordraObject.create(
             obj_type="CreateAction",
             obj_json={
@@ -105,7 +111,7 @@ def create_dataset_from_workflow_artifacts(host, user, password, wfl, artifact_s
         )
         created_ids[action["@id"]] = "CreateAction"
 
-        print("Create Dataset")
+        logger.debug("Create Dataset")
         # TODO derive keywords, name and description from workflow input?
         dataset = cordra.CordraObject.create(obj_type="Dataset", obj_json={
             "name": "Species distribution models for <Enter Species>",
@@ -119,15 +125,16 @@ def create_dataset_from_workflow_artifacts(host, user, password, wfl, artifact_s
         created_ids[dataset["@id"]] = "Dataset"
 
         # Update files parfOf/resultOf to point to dataset/action
-        print("Updating files backref to dataset/action")
-        for id in [id for id in created_ids if created_ids[id] == "FileObject"]:
-            obj = cordra.CordraObject.read(obj_id=id, **upload_kwargs)
+        logger.debug("Updating files backref to dataset/action")
+        for cordra_id in [id for id in created_ids if created_ids[id] == "FileObject"]:
+            obj = cordra.CordraObject.read(obj_id=cordra_id, **upload_kwargs)
             if ("partOf" not in obj) or (obj["partOf"] is None):
                 obj["partOf"] = [dataset["@id"]]
             obj["resultOf"] = action["@id"]
-            cordra.CordraObject.update(obj_id=id, obj_json=obj, **upload_kwargs)
+            cordra.CordraObject.update(obj_id=cordra_id, obj_json=obj, **upload_kwargs)
 
+        logger.info("Dataset ingested. Cordra ID: " + dataset["@id"])
     except Exception as e:
         print(f"Failed to create corda dataset: {type(e)} {str(e)}. Cleaning up uploaded objects")
-        for id in created_ids:
-            cordra.CordraObject.delete(obj_id=id, **upload_kwargs)
+        for cordra_id in created_ids:
+            cordra.CordraObject.delete(obj_id=cordra_id, **upload_kwargs)
